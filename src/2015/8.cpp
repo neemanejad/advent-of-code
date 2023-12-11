cd1347a7fb3f5fb75e641e43c9d29be99aa4f2d6
Neema Nejad <dnnejad@gmail.com>
2023-12-10 21:46:49 -0800

    Finished 8 part 2

--- src/2015/8.cpp
+++ src/2015/8.cpp
@@ -17,7 +17,6 @@ main (void)
     std::string buff;
     while (getline (inputFile, buff))
     {
-        std::cout << buff << std::endl;
         for (size_t i = 0; i < buff.size (); i++)
         {
             auto c = buff[i];
@@ -57,6 +56,38 @@ main (void)
     }
 
     std::cout << charCounter - memCharCounter << std::endl;
+
+    /*
+     * Part 2: Encode original string, get length,
+     * and subtract encoded length from original length.
+     */
+    size_t encodedCharCounter = 0;
+    charCounter = 0;
+    inputFile.clear ();
+    inputFile.seekg (inputFile.beg);
+    while (getline (inputFile, buff))
+    {
+        for (size_t i = 0; i < buff.size (); i++)
+        {
+            charCounter++;
+            encodedCharCounter++;
+            auto c = buff[i];
+
+            switch (c)
+            {
+            case '"':
+            case '\\':
+            {
+                encodedCharCounter++;
+            }
+            }
+        }
+
+        // Add beginning and ending quotations for encoded string
+        encodedCharCounter += 2;
+    }
+
+    std::cout << encodedCharCounter - charCounter << std::endl;
 
     return 0;
 }
